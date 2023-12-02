8824737d073edd80ccbc356f2f7c9336ecebf0eb
Ben Beck <ben.beck.764@gmail.com>
2023-12-02 00:22:31 -0600

    Finally figured this one out... smh

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,15 +1,11 @@
 import Typography from '@mui/material/Typography';
 
 const Home = async () => {
-  const data = process.env;
-  console.log(data);
-
   return (
     <>
       <Typography variant="h1" sx={{ color: { xs: 'red', xl: 'blue' } }}>
         Hello World
       </Typography>
-      <Typography variant="paragraph">{JSON.stringify(data)}</Typography>
     </>
   );
 };
