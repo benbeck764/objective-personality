7c3b783abf83ed0ad725a2a1061c841c954e7cd8
Ben Beck <ben.beck.764@gmail.com>
2023-12-01 23:44:18 -0600

    Test

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,13 +4,14 @@ import { Typography } from '@mui/material';
 const Home = async () => {
   const service = TestService.getInstance();
   const data = await service.test();
+  console.log(data);
 
   return (
     <>
       <Typography variant="h1" sx={{ color: { xs: 'red', xl: 'blue' } }}>
         Hello World
       </Typography>
-      <Typography variant="paragraph">{JSON.stringify(data.resultObject.test1)}</Typography>
+      <Typography variant="paragraph">{data.resultObject.test1}</Typography>
     </>
   );
 };
